564e7d46de93e8c83a22b60d09319ab94796aabd
209 <209@academicos.cesde.edu.co>
2022-09-23 19:25:32 -0500

    Se crea funcion

--- carolinasalazar/funcion1.py
+++ carolinasalazar/funcion1.py
@@ -1,1 +1,11 @@
-#FUNCIONES EN PYTHON
+#FUNCIONES EN PYTHON}
+#LAS FUNCIONES VAN EN VERBOS VAN EN MINUSCULAS
+
+#UNA FUNCION SE DECLARA
+def saludarPersona(nombre):
+    print(f'Hola {nombre}')
+
+#UNA FUNCIONN SE LLAMA 
+nombre="Carolina Salazar"
+saludarPersona(nombre)
+
